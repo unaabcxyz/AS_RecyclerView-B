bbf5e897976de8435e6807e12b7283e7b9931a48
unaabcxyz <tkimhyung1230@gmail.com>
2023-06-08 11:35:22 +0700

    recycler view b

---
+++ README.md
@@ -0,0 +1,1 @@
+# AS_RecyclerView-B
---
+++ app/src/main/java/com/dwiauliaanugerah_202102359/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,41 @@
+package com.dwiauliaanugerah_202102359.recyclerviewb;
+
+public class MahasiswaModel {
+    private String Nim, Nama, JenisKelamin, JP;
+
+    public String getNim() {
+        return Nim;
+    }
+
+    public void setNim(String nim) {
+        Nim = nim;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return JenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        JenisKelamin = jenisKelamin;
+    }
+
+    public String getJP() {
+        return JP;
+    }
+
+    public void setJP(String JP) {
+        this.JP = JP;
+    }
+
+    public MahasiswaModel() {
+
+    }
+}
